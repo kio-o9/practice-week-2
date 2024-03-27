d3fa2d45ba3bf56bcf24361746e608f2f45eaa19
kio-o9 <shin22@kyonggi.ac.kr>
2024-03-27 18:05:18 +0900

    TC-9 create

--- main.c
+++ main.c
@@ -9,3 +9,15 @@ int plus(intx, int y) {
 int div(intx, int y) {
 	return x / y;
 }
+int main() {
+	int x, y, op;
+	printf("Enter two numbers(x, y) : ");
+	scanf("%d %d", &x, &y);
+	printf("\nEnter the operation\n1.plus\n2.minus\n>> ");
+	scanf("%d", &op);
+	int res;
+	if (op == 1) res = plus(x, y);
+	if (op == 2) res = minus(x, y);
+	printf("\n Result is [%d]\n", res);
+	return(0);
+}
